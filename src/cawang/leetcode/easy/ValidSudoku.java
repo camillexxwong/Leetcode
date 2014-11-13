53a4aa503d4f454ce70255075981accf2a97eb25
camillexxwong_v <camillexxwong@gmail.com>
2014-11-14 02:11:23 +0800

    add 46 method 2

--- src/cawang/leetcode/easy/ValidSudoku.java
+++ src/cawang/leetcode/easy/ValidSudoku.java
@@ -1,5 +1,7 @@
 package cawang.leetcode.easy;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.HashSet;
 
@@ -24,8 +26,16 @@ import java.util.HashSet;
 public class ValidSudoku {
 	
 	 public boolean isValidSudoku(char[][] board) {
-		 return (isValidSudoku_1(board));
-		 //return (isValidSudoku_2(board));
+		 //return (isValidSudoku_1(board));
+		 try {
+			return (isValidSudoku_2_bitMask(board));
+		} catch (ClassNotFoundException | NoSuchMethodException
+				| SecurityException | IllegalAccessException
+				| IllegalArgumentException | InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
 	 }
 	/**
 	 * 
@@ -49,6 +59,7 @@ public class ValidSudoku {
     	int m=board[0].length; //row length
     	int n=board.length;
     	char escape='.';
+    	//check row
     	for(int i=0;i<n;i++){
     		//if(hasDuplicates(board[i],0,m-1)) return false;
     		if(hasDuplicates(board,escape, i,0,i,m-1)) {
@@ -56,12 +67,14 @@ public class ValidSudoku {
     			return false;
     		}
     	}
+    	//check column
     	for (int j=0;j<m;j++){
     		if(hasDuplicates(board,escape,0,j,n-1,j)) {
     		//	System.out.println("j: "+j);
     			return false;
     		}
     	}
+    	//check square
     	for(int i=0;i<n;i+=3){
     		for(int j=0;j<m;j+=3){
     			if(hasDuplicates(board,escape,i,j,i+2,j+2)) {
@@ -125,46 +138,145 @@ public class ValidSudoku {
     }
     
     /**
-     * for testing, convert test case on Leetcode to Object
-     * @param s
-     * @return
-     */
-    private char[][] convertStringToMatrix(String s){
-    	char[][] result=new char[9][9];
-    	int count=0;
-    	for(int i=0;i<s.length();i++){
-    		char c=s.charAt(i);
-    		if( (c>='0'&&c<='9')||c=='.'){ //'0', not 0
-    			result[count/9][count%9]=c;
-    			count++;
-    		}
-    	}
-    	return result;
-    }
-    public static void main(String[] args){
-    	ValidSudoku obj=new ValidSudoku();
-    	//s1 is false
-    	//s2 is true
-        //s3 is false
-    	String s1="[\"..5.....6\",\"....14...\",\".........\",\".....92..\",\"5....2...\",\".......3.\",\"...54....\",\"3.....42.\",\"...27.6..\"]";
-    	String s2="[\".87654321\",\"2........\",\"3........\",\"4........\",\"5........\",\"6........\",\"7........\",\"8........\",\"9........\"]";
-    	String s3="[\"..4...63.\",\".........\",\"5......9.\",\"...56....\",\"4.3.....1\",\"...7.....\",\"...5.....\",\".........\",\".........\"]";
-    	char[][] test1=obj.convertStringToMatrix(s1);
-    	char[][] test2=obj.convertStringToMatrix(s2);
-    	
-    	System.out.println(Arrays.deepToString(test1));
-    	System.out.println(obj.isValidSudoku(test1));
-    	
-    	System.out.println(Arrays.deepToString(test2));
-    	System.out.println(obj.isValidSudoku(test2));
-    }
-    
-    /**
-     * ToDo, use bitmask instead of hashset
+	 * same as method1, just call differnt hasDuplicates method
+	 * @param board
+	 * @return
+	 * @throws ClassNotFoundException 
+	 * @throws SecurityException 
+	 * @throws NoSuchMethodException 
+	 * @throws InvocationTargetException 
+	 * @throws IllegalArgumentException 
+	 * @throws IllegalAccessException 
+	 */
+	public boolean isValidSudoku_2_bitMask(char[][] board) throws ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		return isValidSudoku_template(board, "hasDuplicates_bitMask");
+	}
+	/**
+     * Use bitmask instead of hashset
+     * Time: O(n), 仅遍历一次，优于HashSet方法
      * @param board
      * @return
+     * 
+     * use bitmask instead of hashset
+     * for each group (9 cells), set a groupbitmask, for each cell, set a cellbitmask
+     * 
+     * the bitmask will have a length of 9, 
+     * 000000001 means there's one 1, and no others
+     * 000001101 means there's one 1, one 3, one 4, and no others
+     * 
+     * groupbitmask stores the appeared elements in this group so far
+     * cellbitmask stores the current element
+     * if current already in gourp, groupbitmask&cellbitmask!=0
+     * 每次循环即将结束时，更新groupbitmask, 加入当前的cell
+     * groupbitmask=groupbitmask|cellbitmask
+     * 
+     * hint:
+     * https://oj.leetcode.com/discuss/13929/o-n-2-java-solution-with-bitmasks
+     *  for (int j = 0; j < SIZE; ++j) {
+                char cell = board[i][j];
+                if (cell == '.')
+                    continue;
+                int cellBitmask = 1 << (cell - '1');
+                if ((cellBitmask & bitmask) != 0)
+                    return false;
+                bitmask |= cellBitmask;
+            }
      */
-    public boolean isValidSudoku_2(char[][] board){
+    private  boolean hasDuplicates_bitMask(char[][] matrix, char escape, int starti, int startj, int endi,int endj){
+    	int m=endj-startj+1; //row length
+    	int n=endi-starti+1;
+    	int validNums=m*n;
+    	int groupBitMask=0;
+    	for(int i=starti;i<endi+1;i++){ //not <n
+    		for(int j=startj;j<endj+1;j++){
+    			char c=matrix[i][j];
+    			if(c==escape) continue; //!!
+    			int cellBitMask=1<<(c-'1');
+    			if((cellBitMask&groupBitMask)!=0)return true;
+    			groupBitMask|=cellBitMask;
+    		}
+    	}
+    	
     	return false;
     }
+    
+    private boolean isValidSudoku_template(char[][] board, String methodName) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, ClassNotFoundException, NoSuchMethodException, SecurityException{
+       	//get method first, use invoke later
+    	Class c_matrix=(new char[0][0]).getClass();
+    	//Class c_char=Class.forName("java.lang.Character");
+       	Class c_char=char.class;
+    	Class c_int=int.class;
+    	Class[] params=new Class[]{c_matrix,c_char,c_int,c_int,c_int,c_int};
+    	Method method=this.getClass().getDeclaredMethod(methodName, params); //private method should use getDeclaredMethod, not getMethod
+    	
+    	int m=board[0].length; //row length
+    	int n=board.length;
+    	char escape='.';
+    	//check row
+    	for(int i=0;i<n;i++){
+    		if((boolean) method.invoke(this,board,escape, i,0,i,m-1)) return false;
+    	}
+    	//check column
+    	for (int j=0;j<m;j++){
+    		if(hasDuplicates(board,escape,0,j,n-1,j)) return false;
+    	}
+    	//check square
+    	for(int i=0;i<n;i+=3){
+    		for(int j=0;j<m;j+=3){
+    			if(hasDuplicates(board,escape,i,j,i+2,j+2)) return false;
+    		}
+    	}
+    	return true;
+    }
+	/**
+	 * for testing, convert test case on Leetcode to Object
+	 * @param s
+	 * @return
+	 */
+	private char[][] convertStringToMatrix(String s){
+		char[][] result=new char[9][9];
+		int count=0;
+		for(int i=0;i<s.length();i++){
+			char c=s.charAt(i);
+			if( (c>='0'&&c<='9')||c=='.'){ //'0', not 0
+				result[count/9][count%9]=c;
+				count++;
+			}
+		}
+		return result;
+	}
+	public static void main(String[] args){
+		ValidSudoku obj=new ValidSudoku();
+		//s1 is false
+		//s2 is true
+	    //s3 is false
+		String s1="[\"..5.....6\",\"....14...\",\".........\",\".....92..\",\"5....2...\",\".......3.\",\"...54....\",\"3.....42.\",\"...27.6..\"]";
+		String s2="[\".87654321\",\"2........\",\"3........\",\"4........\",\"5........\",\"6........\",\"7........\",\"8........\",\"9........\"]";
+		String s3="[\"..4...63.\",\".........\",\"5......9.\",\"...56....\",\"4.3.....1\",\"...7.....\",\"...5.....\",\".........\",\".........\"]";
+		char[][] test1=obj.convertStringToMatrix(s1);
+		char[][] test2=obj.convertStringToMatrix(s2);
+		
+		System.out.println(Arrays.deepToString(test1));
+		System.out.println(obj.isValidSudoku_1(test1));
+		
+		try {
+			System.out.println(obj.isValidSudoku_2_bitMask(test1));
+		} catch (ClassNotFoundException | NoSuchMethodException
+				| SecurityException | IllegalAccessException
+				| IllegalArgumentException | InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(Arrays.deepToString(test2));
+		System.out.println(obj.isValidSudoku_1(test2));
+		try {
+			System.out.println(obj.isValidSudoku_2_bitMask(test2));
+		} catch (ClassNotFoundException | NoSuchMethodException
+				| SecurityException | IllegalAccessException
+				| IllegalArgumentException | InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
